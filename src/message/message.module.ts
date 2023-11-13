11ef09bbfd74519bfe144f5ad5d41a31b0421403
Bruno Dias <bruno_soares_dias@hotmail.com>
2023-11-13 13:52:43 -0300

    chore: add owner name on create message

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from 'src/entities/message/message.entity';
+import { User } from 'src/entities/user/user.entity';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [MessageController],
   providers: [MessageService],
   exports: [MessageService],
